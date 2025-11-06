a069ee0b748aa17db431bf36650e0c4e97da886c
Zuuumay7 <antoine.bousson@oteria.fr>
2025-11-06 14:57:32 +0100

    123

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,46 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google"; // 🔹 IMPORT
 import "./globals.css";
-import {Navbar} from "@/components/navbar";
+import { Navbar } from "@/components/navbar";
 
+// Geist Sans
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
+// Geist Mono
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
 
-import { Montserrat } from "next/font/google"; // 🔹 IMPORT
-
-// Création de la police
+// Montserrat (la police principale)
 const montserrat = Montserrat({
+    variable: "--font-montserrat",
     subsets: ["latin"],
     weight: ["400", "600"],
 });
 
-
-
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "La Cave - La Garenne",
-  description: "",
+    title: "La Cave - La Garenne",
+    description: "",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      < Navbar />
+    return (
+        <html lang="fr">
+        <body
+            className={`${montserrat.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <Navbar />
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
