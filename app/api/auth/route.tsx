a8b76ca32515f3fca28c940745901d7ac196254c
Zuuumay7 <antoine.bousson@oteria.fr>
2025-11-19 00:35:41 +0100

    ok

--- app/api/auth/route.tsx
+++ app/api/auth/route.tsx
@@ -16,7 +16,6 @@ const handler = NextAuth({
                 const envUser = process.env.ADMIN_USERNAME;
                 const envPass = process.env.ADMIN_PASSWORD;
 
-                // Comparaison simple (OK car tu stockes en .env)
                 if (
                     credentials.username === envUser &&
                     credentials.password === envPass
